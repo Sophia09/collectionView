5ac4f1a2edbed17da53a160ae2577f1152497e94
Sophia09 <lisai121121@gmail.com>
2014-08-07 16:25:51 +0800

    Initial Commit

---
+++ CollectionView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionView
+//
+//  Created by thinkit  on 8/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CollectionView
+//
+//  Created by thinkit  on 8/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
